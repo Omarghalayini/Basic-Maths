fdc25339fdbb2163a495f8e71b7f274998d5c7af
Omar Ghalayini <omarghleba@gmail.com>
2023-01-31 19:45:08 +0000

    My second commit

--- Main.cpp
+++ Main.cpp
@@ -8,8 +8,8 @@ cout <<"Please Enter Two Numbers" << endl;
 int v1 = 0, v2 =0;
 cin >> v1 >> v2;
 
-cout <<"The Multiplication of " << v1 << " and " << v2 
-<< " is " << v1 * v2;
+cout <<"The Addition of " << v1 << " and " << v2 
+<< " is " << v1 + v2;
 
 
 }
